27ac6b7d8ab496a5f47155ffe7b4cbb9ce862a64
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2021-04-13 20:13:35 +0700

    initial commit

---
+++ app/helpers/validation.js
@@ -0,0 +1,41 @@
+const Joi = require("joi");
+
+module.exports = {
+  validationUsers: (users) => {
+    const schema = Joi.object({
+      email: Joi.string().email().required(),
+      password: Joi.string().min(8).required().strict(),
+      phoneNumber: Joi.number().required(),
+      username: Joi.string().required(),
+      firstname: Joi.string().required(),
+      lastname: Joi.string().required(),
+      address: Joi.string().required(),
+      gender: Joi.string().required(),
+      dateOfBirth: Joi.date(),
+    });
+    return schema.validate(users);
+  },
+  validationUsersUpdate: (users) => {
+    const schema = Joi.object({
+      email: Joi.string().email().required(),
+      phoneNumber: Joi.number().required(),
+      username: Joi.string().required(),
+      firstname: Joi.string().required(),
+      lastname: Joi.string().required(),
+      address: Joi.string().required(),
+      gender: Joi.string().required(),
+      dateOfBirth: Joi.date(),
+    });
+    return schema.validate(users);
+  },
+  validationUsersUpdatePassword: (users) => {
+    const schema = Joi.object({
+      password: Joi.string().min(8).required().strict(),
+      confirmPassword: Joi.string()
+        .valid(Joi.ref("password"))
+        .required()
+        .strict(),
+    });
+    return schema.validate(users);
+  },
+};
---
+++ app/routers/usersRouter.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const usersController = require("../controllers/usersController");
+const multer = require("../middlewares/multer");
+const auth = require("../middlewares/auth");
+
+router
+  .get("/", auth.verification(), auth.isAdmin(), usersController.findAll)
+  .get("/find-one", auth.verification(), usersController.findOne)
+  .post("/", multer.uploadImage.single("image"), usersController.create)
+  .get("/auth/verify", usersController.verify)
+  .post("/auth/login", usersController.login)
+  .post("/auth/forgot-password", usersController.forgotPassword)
+  .put("/auth/reset-password", usersController.resetPassword)
+  .put("/:id", multer.uploadImage.single("image"), usersController.update)
+  .put("/edit-password/:id", usersController.updatePassword)
+  .delete("/:id", auth.verification(), auth.isAdmin(), usersController.delete);
+
+module.exports = router;
